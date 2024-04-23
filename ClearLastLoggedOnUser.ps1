d8a9b96f0240617df66badcdcc61e8503fb3018d
Pavel <159088278+Pavl3000@users.noreply.github.com>
2024-04-23 17:25:40 +0300

    Add files via upload

---
+++ ClearLastLoggedOnUser.ps1
@@ -0,0 +1,14 @@
+﻿<#
+This PowerShell script is designed to clear the last logged-on user information from a specified computer
+you will see OTHER USER on log in screen after reboot
+#>
+
+#Define computer name 
+$ComputerName = ""
+
+Invoke-Command -ComputerName $computer -ScriptBlock {
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI /v LastLoggedOnUser /f
+reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI /v LastLoggedOnUserSID /f
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI /v LastLoggedOnUser
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI /v LastLoggedOnUserSID
+}
